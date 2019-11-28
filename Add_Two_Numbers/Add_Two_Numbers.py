9007fe71e61f460a6e8d3d084ac165df93f0d873
Qi Mao <mq958778213@hotmail.com>
2019-11-27 18:25:45 -0600

    11/27

--- Add_Two_Numbers/Add_Two_Numbers.py
+++ Add_Two_Numbers/Add_Two_Numbers.py
@@ -6,17 +6,17 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        carry = 0;
-        res = n = ListNode(0);
+        carry = 0
+        res = n = ListNode(0)
         while l1 or l2 or carry:
             if l1:
                 carry += l1.val
-                l1 = l1.next;
+                l1 = l1.next
             if l2:
-                carry += l2.val;
-                l2 = l2.next;
+                carry += l2.val
+                l2 = l2.next
             carry, val = divmod(carry, 10)
-            n.next = n = ListNode(val);
-        return res.next;
+            n.next = n = ListNode(val)
+        return res.next
                 
             
---
+++ Remove_Nth_Node_From_End_of_List/Remove_Nth_Node_From_End_of_List.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        fast = slow = head
+        #one go fast and other go slow, fast run n time first, when fast end, slow is reach the point.
+        for _ in range(n):
+            fast = fast.next
+        if not fast:
+            return head.next
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return head
